6d5054933c367940021805a93262a7984c98af4a
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-03-18 14:38:55 -0300

    todo list- input validation - wip

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { FlatList, StyleSheet, Text, View} from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, View} from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoitem'
 import AddTodo from './components/addTodo'
@@ -16,12 +16,20 @@ export default function App() {
     })
   }
   const submitHandler = (text) =>{
-    setTodos((prevTodos) =>{
-      return [
-        {text: text, key: Math.random().toString()},
-        ...prevTodos
-      ]
-    })
+
+    if(text.length >3){
+      setTodos((prevTodos) =>{
+        return [
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      })
+    }else{
+      Alert.alert('Alert', 'Todos must be over 3 characters long', [
+        {text: 'OK', onPress:() => console.log('alert closed')}
+      ])
+    }
+
   }
 
   return (
