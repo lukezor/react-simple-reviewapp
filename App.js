f3e887a9db88c2b58b3383d5df57a9df6a539abc
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-03-18 14:18:06 -0300

    todo list- list array - wip

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { FlatList, StyleSheet, Text, View} from 'react-native';
 import Header from './components/header'
+import TodoItem from './components/todoitem'
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,11 @@ export default function App() {
     {text: 'create something', key: '2'},
     {text: 'play something', key: '3'},
   ])
+  const pressHandler = (key) => {
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -19,7 +25,7 @@ export default function App() {
           <FlatList 
             data={todos}
             renderItem={({item})=>(
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
           />
         </View>
