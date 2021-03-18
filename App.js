21e459ab71e97422940a749221194d91fbda541f
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-03-18 14:33:18 -0300

    todo list- list array - wip

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { FlatList, StyleSheet, Text, View} from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoitem'
+import AddTodo from './components/addTodo'
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -14,13 +15,20 @@ export default function App() {
       return prevTodos.filter(todo => todo.key != key)
     })
   }
+  const submitHandler = (text) =>{
+    setTodos((prevTodos) =>{
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
 
   return (
     <View style={styles.container}>
-      {/* header */}
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList 
             data={todos}
