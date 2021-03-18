5a2828521f890a5470e3331c05883ec3f694de1d
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-03-18 11:52:43 -0300

    todo list - wip

--- App.js
+++ App.js
@@ -1,37 +1,29 @@
 import React, {useState} from 'react';
-import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, Text, View} from 'react-native';
+import Header from './components/header'
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'Gabriel', id: '1'},
-    {name: 'Yoshi', id: '2'},
-    {name: 'Mario', id: '3'},
-    {name: 'Luigi', id: '4'},
-    {name: 'Peach', id: '5'},
-    {name: 'Toad', id: '6'},
-    {name: 'Bowser', id: '7'},
+  const [todos, setTodos] = useState([
+    {text: 'buy something', key: '1'},
+    {text: 'create something', key: '2'},
+    {text: 'play something', key: '3'},
   ])
-  const pressHandler = (id) =>{
-    console.log(id)
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
-   
 
   return (
     <View style={styles.container}>
-
-      <FlatList
-        keyExtractor={(item)=> item.id} 
-        numColumns={2}
-        data={people}
-        renderItem={({item})=> (
-          <TouchableOpacity onPress={()=> pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
 
     </View>
   );
@@ -41,16 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop:40,
-    paddingHorizontal:20,
-    // alignItems: 'center',
-    // justifyContent: 'center'
   },
-  item:{
-    marginTop: 24,
-    padding:20,
-    backgroundColor: 'lightblue',
-    fontSize: 24,
-    marginHorizontal:10,
+  content:{
+    padding: 40,
+  },
+  list:{
+    marginTop:20,
   }
 });
